9e56914c556866c36d0a50dc6619b788be989c50
liyunkkk <liyunkkk@users.noreply.github.com>
2026-08-31 08:49:39 +0000

    fix: add repositories to pluginManagement and dependencyResolutionManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = java.net.URI("https://api.xposed.info/") }
+    }
+}
+
 rootProject.name = "Home18Grid"
 include(":app")
